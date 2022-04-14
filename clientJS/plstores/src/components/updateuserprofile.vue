4d609fb378cf5d12a83e6d0ed9587c174ffd9762
pruthvipatel240 <pruthvip.itpath@gmail.com>
2022-04-14 19:00:24 +0530

    login and registration feature added

---
+++ clientJS/plstores/src/components/updateuserprofile.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card>
+        <v-card-title>Edit User-Profile</v-card-title>
+        <v-card-text>
+             <v-form>
+        <v-text-field label="UserName"></v-text-field>
+        <v-text-field label="Password"></v-text-field>
+    </v-form>
+        </v-card-text>
+        <v-card-actions>
+              <v-btn color="primary">Update</v-btn>
+              <v-btn color="primary">Cancel</v-btn>
+              <v-btn color="primary">Back to home</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
