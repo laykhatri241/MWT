c409e1f5102059230fd70609ccb1327d1745a9b5
NikunjGhevariya <nikunjg.itpath@gmail.com>
2022-04-20 18:12:08 +0530

    Adding Stepper and models

---
+++ clientts/mwtclientts/src/store/modules/businessDetailsMaster.ts
@@ -0,0 +1,27 @@
+import { VuexModule, Module, Mutation, Action } from "vuex-module-decorators";
+import api from "@/api";
+import BusinessDetailsMaster from "@/interfaces/businessDetailsMaster";
+
+@Module({ namespaced: true })
+class BusinessDetails extends VuexModule {
+  public businessDetailsMaster: BusinessDetailsMaster = {
+    id: 0,
+    UserID: 0,
+    GSTIN: "",
+    PAN: "",
+  };
+  @Action
+  public async createBusinessDetails(data: BusinessDetailsMaster): Promise<boolean> {
+    return api
+      .post("Business/AddBusinessDetails", data)
+      .then((response) => {
+        response.data.data.id = response.data.id;
+        this.context.commit("createBusiness", response.data.data);
+        return true;
+      })
+      .catch(() => {
+        return false;
+      });
+  }
+}
+export default BusinessDetails
