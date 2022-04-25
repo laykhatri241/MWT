3c348f3222f5d5f90ee392c1c9b5658edabbc410
pruthvipatel240 <pruthvip.itpath@gmail.com>
2022-04-25 18:30:47 +0530

    created helper class

--- clientJS/plstores/src/components/Layout.vue
+++ clientJS/plstores/src/components/Layout.vue
@@ -6,12 +6,12 @@
       color="primary"
       dark
     >
-<!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />-->
+      <!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />-->
 
-      <v-toolbar-title
-        style="width: 350px"
-      >
-        <a href="/" class="white--text" style="text-decoration: none"><v-icon>mdi-basket</v-icon>&nbsp;PNLStores</a>
+      <v-toolbar-title style="width: 350px">
+        <a href="/" class="white--text" style="text-decoration: none"
+          ><v-icon>mdi-basket</v-icon>&nbsp;PNLStores</a
+        >
       </v-toolbar-title>
       <v-text-field
         flat
@@ -22,65 +22,39 @@
         class="hidden-sm-and-down pl-10 ml-4"
       />
       <v-spacer />
-      <v-btn icon @click="showdialog =true" 
-        >
+      <v-btn icon @click="showdialog = true">
         <v-icon>mdi-account-circle</v-icon>
-         
       </v-btn>
-       <login v-if="showdialog " @showpopup="showpopup"/>
-                 
+      <login v-if="showdialog" @showpopup="showpopup" />
+
       <v-btn icon>
-        <v-badge
-          content="2"
-          value="2"
-          color="green"
-          overlap
-        >
+        <v-badge content="2" value="2" color="green" overlap>
           <v-icon>mdi-bell</v-icon>
         </v-badge>
       </v-btn>
-      <v-btn  href="/cart" icon>
-        <v-badge
-          content="2"
-          value="2"
-          color="green"
-          overlap
-        >
+      <v-btn href="/cart" icon>
+        <v-badge content="2" value="2" color="green" overlap>
           <v-icon>mdi-cart</v-icon>
         </v-badge>
       </v-btn>
 
-      <v-switch  @click="darkMode" />
+      <v-switch @click="darkMode" />
     </v-app-bar>
     <v-content>
-      <v-bottom-navigation
-        :value="activeBtn"
-        color="primary"
-        horizontal
-      >
+      <v-bottom-navigation :value="activeBtn" color="primary" horizontal>
         <a href="/" class="v-btn">
           <span>Home</span>
         </a>
         <v-menu open-on-hover offset-y>
-          <template v-slot:activator="{  }">
-            <v-btn >
+          <template v-slot:activator="{}">
+            <v-btn>
               <span>Shop</span>
             </v-btn>
           </template>
-          <v-card
-            class="mx-auto"
-            max-width="344"
-            outlined
-          >
-
-            <v-list-item
-              v-for="(item, index) in items"
-              :key="index"
-             
-            >
+          <v-card class="mx-auto" max-width="344" outlined>
+            <v-list-item v-for="(item, index) in items" :key="index">
               <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
-
           </v-card>
         </v-menu>
         <a href="/product" class="v-btn">
@@ -91,47 +65,29 @@
         </v-btn>
       </v-bottom-navigation>
     </v-content>
-      <router-view/>
-    <v-footer
-      :padless="true"
-    >
-      <v-card
-        flat
-        tile
-        width="100%"
-        class="secondary white--text text-center"
-      >
+    <router-view />
+    <v-footer :padless="true">
+      <v-card flat tile width="100%" class="secondary white--text text-center">
         <v-card-text>
-          <v-btn
-            class="mx-4 white--text"
-            icon
-          >
+          <v-btn class="mx-4 white--text" icon>
             <v-icon size="24px">mdi-home</v-icon>
           </v-btn>
-          <v-btn
-            class="mx-4 white--text"
-            icon
-          >
+          <v-btn class="mx-4 white--text" icon>
             <v-icon size="24px">mdi-email</v-icon>
           </v-btn>
-          <v-btn
-            class="mx-4 white--text"
-            icon
-          >
+          <v-btn class="mx-4 white--text" icon>
             <v-icon size="24px">mdi-calendar</v-icon>
           </v-btn>
-           <v-btn
-            class="mx-4 white--text"
-            icon
-          >
+          <v-btn class="mx-4 white--text" icon>
             <v-icon size="24px">mdi-delete</v-icon>
           </v-btn>
-
         </v-card-text>
 
         <v-card-text class="white--text pt-0">
-        For every sale you miss because you're too enthusiastic, you will miss a hundred because you're not enthusiastic enough.” “Every choice you make has an end result.” "Your attitude, not your aptitude, will determine your altitude."
-        .
+          For every sale you miss because you're too enthusiastic, you will miss
+          a hundred because you're not enthusiastic enough.” “Every choice you
+          make has an end result.” "Your attitude, not your aptitude, will
+          determine your altitude." .
         </v-card-text>
 
         <v-divider></v-divider>
@@ -144,61 +100,52 @@
   </v-app>
 </template>
 <script>
-import Login from './Login.vue'
-    export default {
-     
-    components: { Login },
-    theme: {
-      themes: {
-        light: {
-          primary: "#82b19f",
-          secondary: "#f4bf1c",
-          accent: "#47333c",
-          error: "#dc6276",
-          background: "#F6F5F4",
-          formBackground: "#f7f7f7"
-        },
-        dark: {
-          primary: "#82b19f",
-          background: "#181b1f",
-          formBackground: "#23272b"
-        }
-      }
+import Login from "./Login.vue";
+export default {
+  components: { Login },
+  theme: {
+    themes: {
+      light: {
+        primary: "#82b19f",
+        secondary: "#f4bf1c",
+        accent: "#47333c",
+        error: "#dc6276",
+        background: "#F6F5F4",
+        formBackground: "#f7f7f7",
+      },
+      dark: {
+        primary: "#82b19f",
+        background: "#181b1f",
+        formBackground: "#23272b",
+      },
     },
-    
-        data () {
-            return {
-                items: [
-                    { title: 'T-Shirts' },
-                    { title: 'Jackets' },
-                    { title: 'Shirts' },
-                    { title: 'Jeans' },
-                    { title: 'Shoes' },
-                ],
-                activeBtn: 1,
-                 showdialog :false,
+  },
 
-            }
-            
-        },
-        methods:{
-          showpopup(){
-            
-             this.showdialog =false;
-          },
-               darkMode() {
+  data() {
+    return {
+      items: [
+        { title: "T-Shirts" },
+        { title: "Jackets" },
+        { title: "Shirts" },
+        { title: "Jeans" },
+        { title: "Shoes" },
+      ],
+      activeBtn: 1,
+      showdialog: false,
+    };
+  },
+  methods: {
+    showpopup() {
+      this.showdialog = false;
+    },
+    darkMode() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-    }
+    },
   },
   computed: {
     theme() {
       return this.$vuetify.theme.dark ? "dark" : "light";
-    }
-  }
-
-  }
-        
-        
-    
-    
+    },
+  },
+};
 </script>
