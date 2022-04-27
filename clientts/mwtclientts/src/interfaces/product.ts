15cb66b38728acb48913bcc3a3e4f0c54088a0d4
NikunjGhevariya <nikunjg.itpath@gmail.com>
2022-04-27 10:18:12 +0530

    Add Model and Modules

---
+++ clientts/mwtclientts/src/interfaces/product.ts
@@ -0,0 +1,13 @@
+class Product {
+    id !: number
+    SellerID!: number
+    ProdCompanyName!: string
+    ProdName!: string
+    ProdDetails!: string;
+    ProdImage !:string;
+    ProdPrice !: number;
+    CategoryID !: number;
+  }
+
+  export default  Product 
+  
