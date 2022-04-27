a94605f8f32ca2720d7d3c4ca23e66c1ae4d730a
NikunjGhevariya <nikunjg.itpath@gmail.com>
2022-04-27 18:23:55 +0530

    Add Product on company side

--- clientts/mwtclientts/src/store/modules/product.ts
+++ clientts/mwtclientts/src/store/modules/product.ts
@@ -5,11 +5,54 @@ import Category from "@/interfaces/category";
 
 @Module({ namespaced: true })
 class Products extends VuexModule {
-    products = new Product();
-    categories = new Category();
+  products = new Product();
+  categories = new Category();
 
+  @Action
+  public async AddProduct(data: Product): Promise<any> {
+    data.SellerID = Number(localStorage.getItem("UserID"));
+    return callApi
+      .AsyncPOST("Product/AddProduct", data)
+      .then((response) => {
+        console.log("console",response);
 
-    
+        // response.data.data.id = response.data.id;
+        // this.context.commit("create", response.data.data);
+        return response;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+  @Action
+  public async UpdateProduct(data: any): Promise<any> {
+    data.SellerID = Number(localStorage.getItem("UserID"));
+    return callApi
+      .AsyncPOST("Product/UpdateProduct", data)
+      .then((response) => {
+        console.log(response);
+        //localStorage.getItem('Token')
+        return response;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+  @Action
+  public async Getcategory(data: any): Promise<any> {
+    // data.id = Number(localStorage.getItem("UserID"));
+    return callApi
+      .AsyncPOST("Product/GetCategories",data)
+      .then((response) => {
+        console.log(response);
+        //localStorage.getItem('Token')
+        return response;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+  
 }
 
 export default Products;
