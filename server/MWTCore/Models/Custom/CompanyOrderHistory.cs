7b77d21faf7cf75c21f2306f76352c33df139304
lay khatri <layk.itpath@gmail.com>
2022-05-12 09:48:24 +0530

    HotFix: CompanyOrderHistory model Added

---
+++ server/MWTCore/Models/Custom/CompanyOrderHistory.cs
@@ -0,0 +1,16 @@
+﻿using MWTDbContext.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MWTCore.Models.Custom
+{
+    public class CompanyOrderHistory
+    {
+        public int UserID { get; set; }
+        public int ProductID { get; set; }
+        public int OfferID { get; set; }
+        public int OrderID { get; set; }
+        public int Count { get; set; }
+    }
+}
